1e1d1ae5a3ca767089fe96d8aff32fbac9c123b6
Sumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-03-05 14:09:37 -0500

    Adds actual functionality to the Learn More button. Clicking on the button will now take you to a Google search of the author.

--- quote.js
+++ quote.js
@@ -9,12 +9,16 @@ function getQuote() {
 
     var current = Math.floor(Math.random() * quotes.length);
 
+    // Replace quote text and author
     $(".quote").html(quotes[current]['quoteText']);
     if (quotes[current]['quoteAuthor'] === "") {
       $(".author").html("Unknown");
     } else {
       $(".author").html(quotes[current]['quoteAuthor']);
     }
+
+    // Replace learn more button address with new author
+    $(".learnMore").attr('href', 'https://www.google.com/search?q=' + quotes[current]['quoteAuthor'])
   }
 }
 
