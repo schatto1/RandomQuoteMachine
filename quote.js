083bf751ed6de7e27bcf2e714969f0d4c6957c22
Sumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-03-05 12:05:22 -0500

    Adds XMLHttpRequest with correct github.io address for quotes JSON

--- quote.js
+++ quote.js
@@ -1,3 +1,9 @@
+var requestURL = 'https://schatto1.github.io/RandomQuoteMachine/quotes.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
 $(document).ready(function() {
   $("#getQuote").on("click", function(){
     $(".quote").html("Here is the message");
